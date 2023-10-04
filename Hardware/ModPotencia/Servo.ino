a357995f7cb2746c564dfe5032d497b14fb94686
Ninho <ivanilsonjunior@gmail.com>
2023-10-04 17:14:20 -0300

    Modulo de Potencia com servo

---
+++ Hardware/ModPotencia/Servo.ino
@@ -0,0 +1,43 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+int but;
+
+void setup() {
+  pinMode(8, INPUT_PULLUP);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(115200);
+  Serial.println("Inicializando");
+}
+
+void loop() {
+but = digitalRead(8);
+
+if (but == 0) {
+  abrir();
+  delay(1000);
+} else {
+    fechar();
+}
+
+}
+
+void abrir() {
+    myservo.write(0);              // tell servo to go to position in variable 'pos'
+}
+
+void fechar() {
+    myservo.write(90);              // tell servo to go to position in vari
+}
